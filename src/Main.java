8f6e54adb578e92dcad82ad4f8b017ec25ec565e
Zarmina Desktop <zarminaehsan@icloud.com>
2023-09-15 13:51:21 +0200

    code for example 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Creating a list of array
+        int[] list1 = {16,3,8,9,2,7,6};
+
+        // Creating instance of a class to find the biggest nr differently
+        StorsteTall method1 = new StorsteTall();
+
+        // Finding the biggest number
+        int result = method1.finnIndexNr(list1);
+        System.out.println("Index of biggest number is " + result +
+                " which gives the number " + list1[result] + "." );
     }
 }
---
+++ src/StorsteTall.java
@@ -0,0 +1,30 @@
+public class StorsteTall {
+
+    // Checking empty array
+     static void checkArray(int[] array){
+         if(array.length < 1){
+             throw new java.util.NoSuchElementException("Tabellen er tom!");
+         }
+     }
+
+    // Creating method  to find a biggest nr
+    int finnIndexNr(int[] list){
+
+        // Check if list is zero
+        try{
+            checkArray(list);
+        } catch (java.util.NoSuchElementException e) {
+            System.out.println("Caught an exception: " + e.getMessage());
+        }
+
+        // Finding index nr of a biggest nr
+        int maxNr = 0;
+        for(int i = 1; i < list.length; i++){
+            if(list[maxNr] < list[i]){
+                maxNr = i;
+            }
+        }
+        return maxNr;
+    }
+
+}
