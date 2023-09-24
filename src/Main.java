91fd750f744bbb3189b12ac7e17a3b897386ff38
Zarmina Desktop <zarminaehsan@icloud.com>
2023-09-24 10:45:25 +0200

    Updated code after adding new class and method to find smallest nr from the array

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,26 @@ public class Main {
 
         // Creating a list of array
         int[] list1 = {16,3,8,9,2,17,6};
+        int[] a = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
 
-        // Creating instance of a class to find the biggest nr differently
-        StorsteTall method1 = new StorsteTall();
+        // Creating instance of a class to find the biggest nr
+        StorsteTall maksMetode = new StorsteTall();
 
         // Finding the biggest number
-        int result = method1.finnIndexNr(list1);
-        System.out.println("Index of biggest number is " + result +
-                " which gives the number " + list1[result] + "." );
+        int maksNr = maksMetode.finnMaksIndeks(list1);
+
+        // Creating instance of a class to find the smallest nr
+        MinsteTall minMetode = new MinsteTall();
+
+        // Finding the smallest number
+        int minNr = minMetode.finnMinIndeks(a);
+
+        // Viser storstetallet
+        System.out.println("Index of biggest number is " + maksNr +
+                " which gives the number " + list1[maksNr] + "." );
+
+        // Viser minstetallet
+        System.out.println("Index of smallest number is " + minNr +
+                " which gives the number " + a[minNr] + "." );
     }
 }
--- src/StorsteTall.java
+++ src/StorsteTall.java
@@ -1,14 +1,14 @@
 public class StorsteTall {
 
     // Checking empty array
-     static void checkArray(int[] array){
+     public static void checkArray(int[] array){
          if(array.length < 1){
              throw new java.util.NoSuchElementException("Tabellen er tom!");
          }
      }
 
     // Creating method  to find a biggest nr
-    int finnIndexNr(int[] list){
+    int finnMaksIndeks(int[] list){
 
         // Check if list is zero
         try{
@@ -17,14 +17,15 @@ public class StorsteTall {
             System.out.println("Caught an exception: " + e.getMessage());
         }
 
-        // Finding index nr of a biggest nr
-        int maxNr = 0;
+        // Finding index of the biggest number in the array
+        int maxIndeks = 0;
         for(int i = 1; i < list.length; ++i){
-            if(list[i] > list[maxNr]){
-                maxNr = i;
+            if(list[i] > list[maxIndeks]){
+                maxIndeks = i; // indeksen oppdateres
             }
         }
-        return maxNr;
+        // returnerer indeksen/posisjonen til største verdi
+        return maxIndeks;
     }
 
 }
